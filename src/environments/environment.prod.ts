e40bd732c5f925f18f5a7a7f14e07f8416f681b3
MARCO <marcoperezchavez@gmail.com>
2019-10-24 09:37:39 -0500

    added firebase documentations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBwAhCRCJQPDYtSeij_Kcw5yFDFPHZIWbU',
+    authDomain: 'schools39.firebaseapp.com',
+    databaseURL: 'https://schools39.firebaseio.com',
+    projectId: 'schools39',
+    storageBucket: 'schools39.appspot.com',
+    messagingSenderId: '837580192929',
+    appId: '1:837580192929:web:66049bad939a24f76284e0',
+    measurementId: 'G-L5Y1GRK9NS'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBwAhCRCJQPDYtSeij_Kcw5yFDFPHZIWbU',
+    authDomain: 'schools39.firebaseapp.com',
+    databaseURL: 'https://schools39.firebaseio.com',
+    projectId: 'schools39',
+    storageBucket: 'schools39.appspot.com',
+    messagingSenderId: '837580192929',
+    appId: '1:837580192929:web:66049bad939a24f76284e0',
+    measurementId: 'G-L5Y1GRK9NS'
+  }
 };
 
 /*
