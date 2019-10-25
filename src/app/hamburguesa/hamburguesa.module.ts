7bdfd6e8a3649a3939927c17c682ee7d68dc4412
Marco Perez <marcoperezchavez@gmail.com>
2019-10-24 23:42:23 -0500

    temas contenido

--- src/app/hamburguesa/hamburguesa.module.ts
+++ src/app/hamburguesa/hamburguesa.module.ts
@@ -26,16 +26,16 @@ const routes: Routes = [
     {
       path: 'unidad', loadChildren: '../unidad/unidad.module#UnidadPageModule'
     },
-    { path: 'u1-t1', loadChildren: './u1-t1/u1-t1.module#U1T1PageModule' },
-    { path: 'u1-t2', loadChildren: './u1-t2/u1-t2.module#U1T2PageModule' },
-    { path: 'u2-t1', loadChildren: './u2-t1/u2-t1.module#U2T1PageModule' },
-    { path: 'u2-t2', loadChildren: './u2-t2/u2-t2.module#U2T2PageModule' },
-    { path: 'u3-t1', loadChildren: './u3-t1/u3-t1.module#U3T1PageModule' },
-    { path: 'u3-t2', loadChildren: './u3-t2/u3-t2.module#U3T2PageModule' },
-    { path: 'u4-t1', loadChildren: './u4-t1/u4-t1.module#U4T1PageModule' },
-    { path: 'u4-t2', loadChildren: './u4-t2/u4-t2.module#U4T2PageModule' },
-    { path: 'u5-t1', loadChildren: './u5-t1/u5-t1.module#U5T1PageModule' },
-    { path: 'u5-t2', loadChildren: './u5-t2/u5-t2.module#U5T2PageModule' }
+    { path: 'u1-t1', loadChildren: '../u1-t1/u1-t1.module#U1T1PageModule' },
+    { path: 'u1-t2', loadChildren: '../u1-t2/u1-t2.module#U1T2PageModule' },
+    { path: 'u2-t1', loadChildren: '../u2-t1/u2-t1.module#U2T1PageModule' },
+    { path: 'u2-t2', loadChildren: '../u2-t2/u2-t2.module#U2T2PageModule' },
+    { path: 'u3-t1', loadChildren: '../u3-t1/u3-t1.module#U3T1PageModule' },
+    { path: 'u3-t2', loadChildren: '../u3-t2/u3-t2.module#U3T2PageModule' },
+    { path: 'u4-t1', loadChildren: '../u4-t1/u4-t1.module#U4T1PageModule' },
+    { path: 'u4-t2', loadChildren: '../u4-t2/u4-t2.module#U4T2PageModule' },
+    { path: 'u5-t1', loadChildren: '../u5-t1/u5-t1.module#U5T1PageModule' },
+    { path: 'u5-t2', loadChildren: '../u5-t2/u5-t2.module#U5T2PageModule' }
     // ,
     // {
     //   path: 'contenido', loadChildren: './contenido/contenido.module#ContenidoPageModule' 
